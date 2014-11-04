e739eb225247402a7bbc93cb0f10d618bbfec58a
suve <veg@svgames.pl>
2014-11-04 21:19:57 +0100

    Added some SDL 2.0.1 stuff, bugfix in SDL2_image

--- sdl2.pas
+++ sdl2.pas
@@ -202,9 +202,9 @@ implementation
 //from "sdl_version.h"
 procedure SDL_VERSION(x: PSDL_Version);
 begin
-  x.major := SDL_MAJOR_VERSION;
-  x.minor := SDL_MINOR_VERSION;
-  x.patch := SDL_PATCHLEVEL;
+  x^.major := SDL_MAJOR_VERSION;
+  x^.minor := SDL_MINOR_VERSION;
+  x^.patch := SDL_PATCHLEVEL;
 end;
 
 function SDL_VERSIONNUM(X,Y,Z: UInt32): Cardinal;
@@ -374,6 +374,12 @@ end;
 function SDL_GetEventState(type_: UInt32): UInt8;
 begin
   Result := SDL_EventState(type_, SDL_QUERY);
+end;
+
+// from "sdl_timer.h"
+function SDL_TICKS_PASSED(A, B:UInt32):Boolean;
+begin
+   Result := ((Int64(B) - Int64(A)) <= 0)
 end;
 
 end.
